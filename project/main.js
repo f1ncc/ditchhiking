46034f87accdff3775a0407c1f7722a629dae670
sathya717 <rajavslk@gmail.com>
2020-05-06 01:21:55 +0530

    reset pin

--- project/main.js
+++ project/main.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
+    // The location of home
     var home = {lat: 17.3850, lng: 78.4867};
-    // The map, centered at Uluru
+    // The map, centered at home
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom: 4, center: home});
-    // The marker, positioned at Uluru
+    // The marker, positioned at home
     var marker = new google.maps.Marker({position: home, map: map});
   }
