7262ca05848a1858ea95ae7462f31546f64be0ae
sathya717 <rajavslk@gmail.com>
2020-05-06 01:13:21 +0530

    css with materialize

--- project/index.js
+++ project/index.js
@@ -1,5 +1,37 @@
+// const CLIENT_ID = '592117250627-qolkl5j1dq8rjfda9gn4onmrn225f10i.apps.googleusercontent.com';
+// const authorizeButton = document.getElementById('authorize-button');
+// const signoutButton = document.getElementById('signout-button');
+// function handleClientLoad(){
+//     gapi.load('client:auth2', initClient);
+// }
 
+// function initClient() {
+//     gapi.client.init({
+//         clientId: CLIENT_ID
+//     }).then(() => {
+//         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+//         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//         authorizeButton.onclick = handleAuthClick;
+//         signoutButton.onclick = handleSignoutClick;
+//     });
+// }
 
-fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyAb67kQl6Q96cgLm9Ydhg99G0kz0xEzQAE&callback=initMap",(e) => {
-    console.error(e);
-})
+// function updateSigninStatus(isSignedIn) {
+//     if(isSignedIn) {
+//         authorizeButton.style.display = 'none';
+//         signoutButton.style.display = 'block';
+//     } else {
+//         authorizeButton.style.display = 'block';
+//         signoutButton.style.display = 'none';
+//     }
+// }
+
+// // Handling login
+// function handleAuthClick() {
+//     gapi.auth2.getAuthInstance().signIn();
+// }
+
+// // Handling logout
+// function handleSignoutClick() {
+//     gapi.auth2.getAuthInstance().signOut();
+// }
